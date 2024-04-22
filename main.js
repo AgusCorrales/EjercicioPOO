525376162a88c80ae48e89f1023f72953e04f933
AgusCorrales <aguscorrales1997@gmail.com>
2024-04-22 09:30:25 +0200

    EjercicioPOO

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ class Persona{
         this.genero = genero;
     }
     detalles(){
-        console.log(`El nombre es ${this.nombre} , tiene ${this.edad} años y su genero es ${this.genero}`);
+        return `El nombre es ${this.nombre} , tiene ${this.edad} años y su genero es ${this.genero}`;
     }
 }
 
 let persona = new Persona ("Agustin", 26, "masculino");
 persona.detalles()
+console.log(persona.detalles());
 
 
 
@@ -21,12 +22,14 @@ class Estudiantes extends Persona{
         this.grupo = grupo;
     }
     registrar(){
-        console.log(`Nombre del alumno: ${this.nombre}, edad: ${this.edad}, genero: ${this.genero}, curso: ${this.curso}, grupo: ${this.grupo}`);
+        return `Nombre del alumno: ${this.nombre}, edad: ${this.edad}, genero: ${this.genero}, curso: ${this.curso}, grupo: ${this.grupo}`;
     }
 }
 
 let estudiante = new Estudiantes ("Agustin", 26, "Masculino", "Full Stack", "Full Time");
 estudiante.registrar()
+console.log(estudiante.registrar());
+
 
 
 class Profesor extends Persona{
@@ -36,9 +39,10 @@ class Profesor extends Persona{
         this.nivel = nivel;
     }
     asignar(){
-        console.log(`Nombre del profesor: ${this.nombre}, edad: ${this.edad}, genero: ${this.genero}, asignatura: ${this.asignatura}, nivel: ${this.nivel}`);
+        return `Nombre del profesor: ${this.nombre}, edad: ${this.edad}, genero: ${this.genero}, asignatura: ${this.asignatura}, nivel: ${this.nivel}`;
     }
 }
 
 let profesor = new Profesor ("Pepito", 50, "Masculino", "Matemáticas", "Primario");
 profesor.asignar()
+console.log(profesor.asignar());
